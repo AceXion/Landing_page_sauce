dde79cbdafda0abb4a7fbab54d8ed7593cf69c54
Xion <104543741+AceXion@users.noreply.github.com>
2022-11-05 23:07:05 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ $('.head a').on('click', function(e){
 
         e.preventDefault();
         const hash = this.hash
+        console.log(hash)
         $('html, body').animate(
             {
                 scrollTop: $ (hash).offset().top},
@@ -12,3 +13,35 @@ $('.head a').on('click', function(e){
         )
     }
 });
+
+const menu = document.querySelector('#menu')
+const burger = document.querySelector('.hamburger')
+const link = document.querySelector('.links')
+
+function myFunction(listen) {
+    if (listen.matches) {
+        menu.addEventListener('click', ()=>{
+            link.style.display='block';
+            burger.style.display='none'
+        })
+        link.addEventListener('click', ()=>{
+            link.style.display='none'
+            burger.style.display='block'
+        })
+    }
+    else{
+        menu.addEventListener('click', ()=>{
+            link.style.display='flex';
+            burger.style.display='none'
+        })
+        link.addEventListener('click', ()=>{
+            link.style.display='flex'
+            burger.style.display='none'
+        })
+    }
+  }
+
+
+const listen = window.matchMedia('(max-width:734px)')
+myFunction(listen)
+listen.addListener(myFunction)
